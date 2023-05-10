5426d30c7993d17f9ef613735a8c33063a66b74f
HernanEka <hernandaeka3@gmail.com>
2023-05-10 11:55:57 +0700

    09_Data_Comunity_Update

--- resources/views/Admin/Detail_Comunity.blade.php
+++ resources/views/Admin/Detail_Comunity.blade.php
@@ -2,20 +2,21 @@
 
 @section('konten')
     <section id="DetailUser" class="min-vh-100">
-        <div class="container my-5 py-5">
-            <h1 class="text-white">Detail User</h1>
+        <div class="container my-5 py-5 text-center">
+            <h1 class="text-white">Detail Comunity</h1>
             <div class="bg-secondary p-5 text-white">
-                <div class="row align-items-center">
-                    <div class="col-md-3">
-                        <i class="bi bi-person-circle display-1"></i>
-                    </div>
-                    <div class="col">
+                <div class="text-center">
+                    <i class="bi bi-person-circle display-1"></i>
+                    <div class="mb-5">
                         <h1 class="display-6">Nama Komunitas : {{ $user->username }}</h1>
                         <h5 class="fw-light">Email Komunitas : {{ $user->email }}</h5>
                         <h5 class="fw-light">Tanggal Komunitas Daftar : {{ $user->created_at }}</h5>
                     </div>
-                    <div class="border-start col-md-5">
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, aut. Perferendis explicabo id amet a harum animi, expedita ducimus quisquam tempore iusto sequi nemo aperiam tenetur minima mollitia distinctio eaque vitae quibusdam cumque! Animi, eaque sint optio a repudiandae ipsum adipisci saepe suscipit exercitationem molestias, itaque commodi voluptate laborum fugit.</p>
+                    <div class="mb-3">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nostrum, aut. Perferendis explicabo id
+                            amet a harum animi, expedita ducimus quisquam tempore iusto sequi nemo aperiam tenetur minima
+                            mollitia distinctio eaque vitae quibusdam cumque! Animi, eaque sint optio a repudiandae ipsum
+                            adipisci saepe suscipit exercitationem molestias, itaque commodi voluptate laborum fugit.</p>
                     </div>
                 </div>
             </div>
